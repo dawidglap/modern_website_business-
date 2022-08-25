43eba81c7fc577835143ceaf386a482928e46217
Dawid Glapiak <dg18@live.it>
2022-08-25 16:53:29 +0200

    components imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        HERO
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        STATS
+        business
+        billing
+        card deal
+        testimonial
+        clients
+        cta
+        footer
+      </div>
+    </div>
+
+  </div>
+);
 
 export default App
